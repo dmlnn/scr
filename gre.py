d187cd250accc6ca53c1de2eecbde531c8cd8634
dmlnn <94437083+dmlnn@users.noreply.github.com>
2023-04-12 12:22:08 +0300

    Update gre.py

--- gre.py
+++ gre.py
@@ -5,7 +5,7 @@ input('''
 !!! Pered ispolzovaniem scripta ybedis 4to na vtoroi mashine nastroen ssh !!!
 !!! Dlya korrektnoi raboti scripta, pered ego ispolzovaniem               !!! 
 !!! vipolni pervoe podklychenie po ssh k clienty sam(ssh root@IP-CLIENTA) !!!
-!!! 4tobi zakrit script najmi Ctrl+C, 4tobi prodoljit lybyu klavishy      !!!
+!!! 4tobi zakrit script najmi Ctrl+C, 4tobi prodoljit enter               !!!
 Nastroika ssh na cliente:
 apt install ssh -y
 nano /etc/ssh/sshd_config
@@ -15,7 +15,7 @@ systemctl restart ssh
 !!! Pered ispolzovaniem scripta ybedis 4to na vtoroi mashine nastroen ssh !!!
 !!! Dlya korrektnoi raboti scripta, pered ego ispolzovaniem               !!! 
 !!! vipolni pervoe podklychenie po ssh k clienty sam(ssh root@IP-CLIENTA) !!!
-!!! 4tobi zakrit script najmi Ctrl+C, 4tobi prodoljit lybyu klavishy      !!!
+!!! 4tobi zakrit script najmi Ctrl+C, 4tobi prodoljit enter               !!!
 ''')
 local_ip = input("IP etoi mashini: ")
 endpoint_ip = input("IP kone4noi mashini: ")
