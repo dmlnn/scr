2a62299c98f760acf52148e927f0054788e14cb4
dmlnn <94437083+dmlnn@users.noreply.github.com>
2023-04-13 14:38:44 +0300

    Update routnat.py

--- routnat.py
+++ routnat.py
@@ -2,8 +2,8 @@ import os
 import ipaddress
 
 oc = input('Настраиваем debian или centos?(1/2): ')
-networks = input('Введи все 4 подсети слева направо через пробел(x.x.x.0 x.x.x.0...)')
-l_or_r = input("Eto leviy ili praviy router?(1/2): ")
+networks = input('Введи все 4 подсети слева направо через пробел по образцу(x.x.x.0 x.x.x.0...)')
+l_or_r = input("Это левый или правый роутер?(1/2): ")
 
 ip1 = networks.split()[0]
 ip2 = networks.split()[1]
@@ -31,7 +31,7 @@ up ip route add {ip1 + '/24'} via {ip_rt}
 
 
 def ostalnoe_deb():
-	nat = input("nastroit nat?(y or n)")
+	nat = input("Настроить nat?(y/n)")
 
 	if nat == "y":
 		os.system("apt install iptables -y")
@@ -39,7 +39,7 @@ def ostalnoe_deb():
 		os.system("DEBIAN_FRONTEND=noninteractive apt install iptables-persistent")
 		os.system("systemctl restart netfilter-persistent")
 		print("nat nastoren")
-	else: print("ny lan")
+	else: print("Ну лан")
 
 	file3 = open('/etc/sysctl.conf','a')
 	file3.write("net.ipv4.ip_forward=1")
@@ -48,28 +48,32 @@ def ostalnoe_deb():
 
 	reb = input("reboot?(y)")
 	if reb =="y": os.system("reboot")
-	else: print("Ne zabyd rebootnyt")
+	else: print("Не забудь перезагрузить")
 
 # ФУНКЦИИ ДЛЯ ЦЕНТОСА
 
 def left_cent():
 	ip_rt = str(ipaddress.IPv4Address(ip2) + 1)
-	with open('/etc/network/interfaces', 'a') as f:
-		f.write(f'''
+	with open('/etc/rc.local', 'a') as f:
+		routs = (f'''
 up ip route add {ip3 + '/24'} via {ip_rt}
 up ip route add {ip4 + '/24'} via {ip_rt}
 ''')
+		f.write("\n{" + routs + "\n}" )
+	os.system('chmod +x /etc/rc.d/rc.local')
 
 def right_cent():
 	ip_rt = str(ipaddress.IPv4Address(ip3) + 1)
-	with open('/etc/network/interfaces', 'a') as f:
-		f.write(f'''
+	with open('/etc/rc.local', 'a') as f:
+		routs = (f'''
 up ip route add {ip2 + '/24'} via {ip_rt}
 up ip route add {ip1 + '/24'} via {ip_rt}
 ''')
+		f.write("\n{" + routs + "\n}" )
+	os.system('chmod +x /etc/rc.d/rc.local')
 
 def ostalnoe_cent():
-	nat = input("nastroit nat?(y or n)")
+	nat = input("Настроить nat?(y/n)")
 
 	if nat == "y":
 		os.system("apt install iptables -y")
@@ -77,7 +81,7 @@ def ostalnoe_cent():
 		os.system("DEBIAN_FRONTEND=noninteractive apt install iptables-persistent")
 		os.system("systemctl restart netfilter-persistent")
 		print("nat nastoren")
-	else: print("ny lan")
+	else: print("Ну лан")
 
 	file3 = open('/etc/sysctl.conf','a')
 	file3.write("net.ipv4.ip_forward=1")
@@ -86,7 +90,7 @@ def ostalnoe_cent():
 
 	reb = input("reboot?(y)")
 	if reb =="y": os.system("reboot")
-	else: print("Ne zabyd rebootnyt")
+	else: print("Не забудь перезагрузить")
 
 if oc == "1":
 	if l_or_r == "1": left_deb(); ostalnoe_deb()
