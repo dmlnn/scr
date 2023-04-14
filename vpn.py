50d81838abfb1423aec936993a6f1349dbd265f8
dmlnn <94437083+dmlnn@users.noreply.github.com>
2023-04-14 11:34:28 +0300

    Update vpn.py

--- vpn.py
+++ vpn.py
@@ -19,15 +19,15 @@ systemctl restart ssh
 !!! подключать клиентов придется самому (ssh root@IP-CLIENTA)            !!!
 !!! Остановить скрипт - Ctrl+C, продолжить - Enter                       !!!
 ''')
-srv_vpn = input('Сервер впн будет на этой машине?(y/n)')
+
 print('Введи айпи адрес сервера для впн если он на этой машине, то адрес этой машины: ')
 ip_srv = input()
 ip_cl = input('Введи ip vpn-клиента: ')
 print('Введи ip для самого vpn обязательно с 0 на конце, например 5.5.5.0: ')
 ip_vpn = input()
-print('Введи ос сервера и клиента маленькими буквами(debian/centos)')
-os_vpn_srv = input('Server: ')
-os_vpn_cl = input('Client: ')
+#print('Введи ос клиента маленькими буквами(debian/centos)')
+#os_vpn_cl = input('Client: ')  
+                                           # Конфиги впна для сервера и клиента 
 params_vpn_serv = (f'''port 1122
 proto udp
 dev tun
@@ -75,24 +75,37 @@ tls-auth /etc/openvpn/client/ta.key 1
 remote-cert-tls server
 cipher AES-128-CBC
 auth SHA256''')
-                                           # Подготовка для впна на дебиане
-install_vpn_deb =(f'''import os
+
+
+
+                                           # Настройка впна на дебиане если он сервер
+install_vpn_deb_srv =(f'''import os
 
 os.system('apt install openvpn -y')
-os.system('zcat /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf')
-with open('/etc/openvpn/server.conf') as f:
-    f.write('')
+with open('/etc/openvpn/server.conf', 'w') as f:
+    f.write('{params_vpn_serv}')
+os.system('systemctl restart openvpn@server')
 ''')
 
-                                           # Подготовка для впна на центосе
-install_vpn_cent =()
+                                           # Настройка впна на дебиане если он клиент
+install_vpn_deb_cl =(f'''import os
 
-def yd_server():                           # Если сервер впн не совпадает с центром сертификации
-  ()
-def server_vpn():                          # Если сервер впн совпадает с центром сертификации
-  ()
-if srv_vpn == 'y': server_vpn()
-else: yd_server()
+os.system('apt install openvpn -y')
+with open('/etc/openvpn/client.conf', 'w+') as f:
+    f.write('{params_vpn_cl}')
+os.system('systemctl restart openvpn@client')
+''')
+
+                                           # Настройка впна на центосе клиенте
+install_vpn_cent_cl =(f'''import os
+
+os.system('yum install epel-release -y')
+os.system('yum update -y')
+os.system('yum install openvpn -y')
+with open('/etc/openvpn/client.conf', 'w+') as f:
+    f.write('{params_vpn_cl}')
+os.system('systemctl restart openvpn@client')
+''')
 
 def cert():                                 # Центр сертификации
   os.system('yum install epel-release -y')
@@ -131,5 +144,31 @@ set_var EASYRSA_DIGEST            "sha512"''')
   os.system('openvpn --genkey --secret ta.key')
   os.chdir('/root')
 
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/ca.crt root@{ip_srv}:/etc/openvpn/server')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/dh.pem root@{ip_srv}:/etc/openvpn/server')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/issued/serv.crt root@{ip_srv}:/etc/openvpn/server')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/private/serv.key root@{ip_srv}:/etc/openvpn/server')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/ta.key root@{ip_srv}:/etc/openvpn/server')
+  
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/ca.crt root@{ip_cl}:/etc/openvpn/client')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/issued/client.crt root@{ip_cl}:/etc/openvpn/client')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/private/client.key root@{ip_cl}:/etc/openvpn/client')
+  os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/ta.key root@{ip_cl}:/etc/openvpn/client')
 
-os.system(f'sshpass -proot scp /etc/openvpn/easy-rsa/3/pki/ca.crt root@{ip_cl}:/etc')
+def scr_srv():                                      # Создание и отправка агента на сервер впн
+  with open('scr-main/agent_vpn_srv', 'w+') as f:
+    f.write(install_vpn_deb_srv)
+  os.system(f'sshpass -proot scp scr-main/agent_vpn_srv root@{ip_srv}:/etc')
+  os.system(f'sshpass -proot ssh root@{ip_srv} python3 /etc/agent_vpn_srv')
+
+def scr_cl():                                       # Создание и отправка агента на клиента впн
+  with open('scr-main/agent_vpn_cl', 'w+') as f:
+    f.write(install_vpn_cent_cl)
+  os.system(f'sshpass -proot scp scr-main/agent_vpn_srv root@{ip_cl}:/etc')
+  os.system(f'sshpass -proot ssh root@{ip_cl} python3 /etc/agent_vpn_cl')
+
+scr_srv()
+scr_cl()
+cert()
+
+print('VPN настроен')
