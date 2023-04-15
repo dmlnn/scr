295156eab14b0de69e1ea760caeb1202e437cf59
dmlnn <94437083+dmlnn@users.noreply.github.com>
2023-04-15 22:21:10 +0300

    Update RSYS_SRV.py

--- RSYS_SRV.py
+++ RSYS_SRV.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 #################### для сервера
 import os
+
+
+
 listok = []
 
-listok += [input('vvedite HOSTNAME 1-ogo (iz treh) clienta ')]
-listok += [input('vvedite HOSTNAME 2-ogo (iz treh) clienta ')]
-listok += [input('vvedite HOSTNAME 3-ogo (iz treh) clienta ')]
+listok += [input('Введи HOSTNAME первого (из трех) клиента: ')]
+listok += [input('Введи HOSTNAME второго (из трех) клиента: ')]
+listok += [input('Введи HOSTNAME первого (из трех) клиента: ')]
+ip_srv = input('Введи айпи сервера для логов: ')
 
 with open('/etc/rsyslog.conf', 'r') as f:
   old = f.read()
@@ -23,3 +27,25 @@ if ( $hostname == "{listok[i]}"  and $syslogseverity-text == “err” ) then /o
 ''')
 
 os.system('systemctl restart rsyslog')
+
+agentik = (f'''# -*- coding: utf-8 -*-
+import os
+
+with open('/etc/rsyslog.conf', 'a') as f:
+  f.write(f'''
+*.=crit @{ ip_srv }
+*.=err @{ ip_srv }
+*.=info @{ ip_srv }
+''')
+
+os.system('systemctl restart rsyslog')''')
+
+with open('scr-main/agent_syslog', 'w+') as f:
+  f.write(agentik)
+
+os.system('apt install sshpass -y')
+os.system('yum install sshpass -y')
+
+os.system(f'sshpass -proot scp scr-main/agent_syslog root@{listok[0]}')
+os.system(f'sshpass -proot scp scr-main/agent_syslog root@{listok[1]}')
+os.system(f'sshpass -proot scp scr-main/agent_syslog root@{listok[2]}')
