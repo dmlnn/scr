e0d74d8fa40c05a642801cfadb581923e3bffa28
dmlnn <94437083+dmlnn@users.noreply.github.com>
2023-04-16 13:42:40 +0300

    Update rsyslog.py

--- rsyslog.py
+++ rsyslog.py
@@ -17,12 +17,14 @@ for i in ip_cl:
   out_h =out_h[:-3]
   listok += [out_h]
 
+
+  
 with open('/etc/rsyslog.conf', 'r') as f:
   old = f.read()
 
 with open('/etc/rsyslog.conf', 'w') as f:
-  new1 = old.replace('#module(load=”imudp”)', 'module(load=”imudp”)')
-  new2 = new1.replace('#input(type=”imudp” port=”514”)', 'input(type=”imudp” port=”514”)')
+  new1 = old.replace('#module(load="imudp")', 'module(load="imudp")')
+  new2 = new1.replace('#input(type="imudp" port="514")', 'input(type="imudp" port="514")')
   new3 = new2.replace('#$ModLoad imudp', '$ModLoad imudp')
   new4 = new3.replace('#$UDPServerRun 514', '$UDPServerRun 514')
   f.write(new4)
@@ -31,8 +33,8 @@ for i in range(len(listok)):
   with open('/etc/rsyslog.conf', 'a') as f:
     f.write(f'''
 if ( $hostname == "{listok[i]}" and $syslogseverity == 2 ) then /opt/logs/{listok[i]}/crit.log
-if ( $hostname == "{listok[i]}" and $syslogfacility-text == “auth” ) then /opt/logs/{listok[i]}/auth.log
-if ( $hostname == "{listok[i]}"  and $syslogseverity-text == “err” ) then /opt/logs/{listok[i]}/error.log
+if ( $hostname == "{listok[i]}" and $syslogfacility-text == "auth" ) then /opt/logs/{listok[i]}/auth.log
+if ( $hostname == "{listok[i]}"  and $syslogseverity-text == "err" ) then /opt/logs/{listok[i]}/error.log
 ''')
 
 os.system('systemctl restart rsyslog')
